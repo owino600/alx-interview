78408c9851b965b4dc7003eafeeaf7c7a93c20c0
owino600 <owinodaniel600@gmail.com>
2024-04-25 19:10:40 +0300

    .\0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]  # First element of each row is always 1
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)  # Last element of each row is always 1
+        triangle.append(row)
+
+    return triangle
+
+# Test the function
+pascal_triangle_5 = pascal_triangle(5)
+for row in pascal_triangle_5:
+    print(row)
